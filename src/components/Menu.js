a69cb02a4eeec6ac14f3703cd076e27e4de13553
ASHFAK <ashfakv17@gmail.com>
2023-01-17 17:01:58 +0530

    going to categorizing

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,28 +3,45 @@
 import React, { useEffect, useState } from "react";
 import Hero  from "./Hero";
 import SpecialDishes from "./SpecialDishes";
+import FilteredDishes from "./FilteredDishes";
 function Menus(){
 
     let [menu,SetMenu] = useState([])
-
+    let [categories ,SetCategory] = useState([])
 
    async function  getAllMenus(){
-        const API_URL ="https://www.themealdb.com/api/json/v1/1/search.php?f=a"
+        const API_URL ="https://www.themealdb.com/api/json/v1/1/search.php?f=C"
+ 
+
          let response = await fetch(API_URL)
          let data = await  response.json()
          SetMenu (data.meals)
       
     }
-   console.log("menus are",menu);
+
+    
+   async function  getAllCategories(){
+    const API_URL ="https://www.themealdb.com/api/json/v1/1/categories.php"
+     let response = await fetch(API_URL)
+     let categoryData = await  response.json()
+     SetCategory (categoryData.categories)
+  
+}
+
+
+console.log(SetCategory);
+   
+
     useEffect (()=>{
-getAllMenus()
+getAllMenus();
+getAllCategories();
     },[])    
 
-    let menus = menu.map((item)=>{
+ menu.map((item)=>{
 console.log(item);
         return(
 <>
-               <img src={item.strMealThumb} alt="" />
+               <img src={item.strMealThumb}  alt="" />
            <h1>{item.strMeal}</h1>
 
     </>    
@@ -39,6 +56,7 @@ console.log(item);
         <>
    <Hero />
     <SpecialDishes SpecialDish={menu}/>
+    <FilteredDishes  categoriesData ={categories} allMenus={menu}/>
         </>
     )
 }
